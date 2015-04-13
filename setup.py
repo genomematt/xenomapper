f2af470a9313a4cc9fde5a6f936b36e885ade4be
Matthew Wakefield <MattJWakefield@gmail.com>
2015-04-13 14:10:57 +1000

    bump version to 1.0b3 and update test hashes

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ except ImportError:
 
 setup(
     name='XenoMapper',
-    version='1.0b2',
+    version='1.0b3',
     author='Matthew Wakefield',
     author_email='matthew.wakefield@unimelb.edu.au',
     packages=['xenomapper'],
